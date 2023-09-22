62c0876e031a9fdd1865f8c36eea6da7b4dfccb4
Ali Hassan Cheema <acheema152@yahoo.com>
2023-09-22 03:49:46 +0200

    modified multer configuration

--- backend/middlewares/multer-config.js
+++ backend/middlewares/multer-config.js
@@ -12,13 +12,38 @@ const storage = multer.diskStorage({
   },
   filename: (req, file, callback) => {
     const name = file.originalname.split(" ").join("_").split(".")[0];
-    console.log(file.mimetype);
-    const extension = MIME_TYPES[file.mimetype];
-    console.log(MIME_TYPES[file.mimetype]);
-    console.log(extension);
-    callback(null, name + Date.now() + "." + extension);
-  },
 
+    try {
+      if (typeof MIME_TYPES[file.mimetype] !== "undefined") {
+        const extension = MIME_TYPES[file.mimetype];
+        callback(null, name + Date.now() + "." + extension);
+      } else {
+        const error = new Error("Invalid file type");
+        error.message =
+          "This type of photo is not allowed. Please attach a JPG or PNG.";
+        callback(error, null);
+      }
+    } catch (error) {
+      callback(error, null);
+    }
+  },
 });
 
-module.exports = multer({ storage: storage }).single("image");
+module.exports = (req, res, next) => {
+  const upload = multer({
+    storage: storage,
+  }).single("image");
+
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      // A Multer error occurred (e.g., file size limit exceeded)
+      res.status(400).json({ message: "Multer error: " + err.message });
+    } else if (err) {
+      // An error occurred during file upload (e.g., invalid file type)
+      res.status(400).json({ message: err.message });
+    } else {
+      // File upload successful
+      next();
+    }
+  });
+};
